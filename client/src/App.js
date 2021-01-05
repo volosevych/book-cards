d19dd203ad8a7a22085d6c3187942849bad6a980
Anastasia Volosevych <volosevichh@gmail.com>
2021-01-05 14:38:52 -0700

    added the font-awesome, google-fonst links

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from './components/Header';
+import Jumb from './components/Jumbotron';
 
-
-function App() {
-  return (
-    <div><h1>hi</h1></div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header/>
+          <Jumb/>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;

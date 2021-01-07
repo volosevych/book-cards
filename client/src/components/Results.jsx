836293b46ddb2af1dd95a7ae28bf6e8ddb6daf9b
Anastasia Volosevych <volosevichh@gmail.com>
2021-01-07 16:24:41 -0700

    removed bug

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import API from '../utils/API';
-import '../CSS/Results.css';
+import API from "../utils/API";
 
 class Results extends Component {
 
@@ -31,7 +30,7 @@ class Results extends Component {
         return (
             <div>
                 {!this.props.books.length ? (
-                    <h1 className="text-center display-results">Oops... No Results to Display</h1>
+                    <h1 className="text-center">No Results to Display</h1>
                 ) : (
                         <div>
                             {this.props.books.map(result => (
@@ -45,7 +44,7 @@ class Results extends Component {
                                                 <h5 className="card-title">{result.title} by {result.authors}</h5>
                                                 <p className="card-text">{result.description}</p>
                                                 <div>
-                                                    <a href={result.link} className="btn badge-pill btn-outline-dark mt-3" target="__blank" >View</a>
+                                                    <a href={result.link} className="btn badge-pill btn-outline-dark mt-3" target="_blank" >View</a>
                                                     <button onClick={() => this.handleSave(result)} className="btn badge-pill btn-outline-warning mt-3 ml-3" >
                                                         {this.state.savedBooks.map(book => book._id).includes(result._id) ? "Unsave" : "Save"}
                                                     </button>
